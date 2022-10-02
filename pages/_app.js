d9e268bb5d8e5a52519c5cb05ddfd317e0361435
SabrenaArref <112998673+SabrenaArref@users.noreply.github.com>
2022-10-02 22:25:58 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Create List People</title>
+				<title>KIRATECH Assignment</title>
 				<link rel="icon" href="/favicon.ico" />
 				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous" />
 				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossOrigin="anonymous"></script>
